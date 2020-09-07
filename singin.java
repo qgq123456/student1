615f91f9a217ef136e737647f2fbdcfb666029f6
qgq <qgq@qq.com>
2020-09-07 17:02:19 +0800

    在家开发其他签到功能

---
+++ singin.java
@@ -0,0 +1,10 @@
+public class singin{
+	private int id;
+	private String age;
+	public void sign(){
+		System.out.println("在家开发的功能");
+	}
+	public void sign(int id){
+			System.out.println("在家开发的代码");
+	}
+}
